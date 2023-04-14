c2d49815b8da471e938638750afb32ebe532bbd1
JaeHoon Shin <shinj@korea.kr>
2023-04-15 08:38:59 +0900

    Update API_smartfarmkorea.py

--- API_smartfarmkorea.py
+++ API_smartfarmkorea.py
@@ -1,29 +1,13 @@
 # pip install requests
+import pandas as pd
 import requests
 
 import json
 
-'''
-api uri
-/api/{read_api_key}/{date}/{hour}
-/api/w4460bcc0494f41f6a3af01e54bd80c13/20191121/13
-
-success response
-{
-    "datas":
-        [
-            {"dt":"2019-11-21 13:41:55","dry_bulb_tp":"25","wet_bulb_tp":"15","solrad_qy":"20"},
-            {"dt":"2019-11-21 13:42:13","dry_bulb_tp":"25","wet_bulb_tp":"15","solrad_qy":"20"},
-            ...
-        ],
-    "resultCode":"",
-    "resultMessage":""
-}
-'''
+import datetime as dt
 
 # server URL
-
-serviceKey = "6f9dcd925fa44a949179d2ea967599e8"
+serviceKey = "53bcfc152294461dbe757b8a7b185c46"
 
 # data logging date and time
 
@@ -45,18 +29,92 @@ json_obj = json.loads(response.text)
 
 api_url = "http://www.smartfarmkorea.net/Agree_WS/webservices/InnovationValleyRestService/getCroppingSeasonDataList"
 
-for i in range(0, 56):
+for i in range(0, 1):
     userid = json_obj[i]['userId']
     fcltyId =json_obj[i]['fcltyId']
-    print(userid, " ", fcltyId)
-    api_uri = f"{api_url}/{serviceKey}/{fcltyId}/{userid}"
-    response = requests.get(api_uri, verify=False)
-    print(response.text)
 
-measDate = "20220703"
+api_uri = f"{api_url}/{serviceKey}/{fcltyId}/{userid}"
+response = requests.get(api_uri, verify=False)
+#print(response.text)
+
+measDate = "20230414"
 api_url = "http://www.smartfarmkorea.net/Agree_WS/webservices/InnovationValleyRestService/getEnvDataList"
 api_uri = f"{api_url}/{serviceKey}/{fcltyId}/{measDate}"
 response = requests.get(api_uri, verify=False)
-print(response.text)
 
+#print(response.text)
+
+json_obj = json.loads(response.text)
+
+dt_now = dt.datetime.strptime(json_obj[0]['measDate'], "%Y-%m-%d %H:%M:%S")
+
+TI = list()
+for i in range(0, len(json_obj)) :
+    d = dict()
+    if json_obj[i]['fatrCode'] == "TI" :
+        d['measDate'] = json_obj[i]['measDate']
+        d['TI'] = float(json_obj[i]['senVal'])
+        TI.append(d)
+
+HI02 = list()
+for i in range(0, len(json_obj)) :
+    d = dict()
+    if json_obj[i]['fatrCode'] == "HI02" :
+        d['measDate'] = json_obj[i]['measDate']
+        d['HI02'] = float(json_obj[i]['senVal'])
+        HI02.append(d)
+
+CI = list()
+for i in range(0, len(json_obj)) :
+    d = dict()
+    if json_obj[i]['fatrCode'] == "CI" :
+        d['measDate'] = json_obj[i]['measDate']
+        d['CI'] = float(json_obj[i]['senVal'])
+        CI.append(d)
+
+SR = list()
+for i in range(0, len(json_obj)) :
+    d = dict()
+    if json_obj[i]['fatrCode'] == "SR" :
+        d['measDate'] = json_obj[i]['measDate']
+        d['SR'] = float(json_obj[i]['senVal'])
+        SR.append(d)
+
+TI_1 = list()
+TI_2 = list()
+for i in range(0, len(TI)):
+    TI_1.append(TI[i]['measDate'])
+    TI_2.append(TI[i]['TI'])
+
+TI_3 = dict()
+TI_3['measDate'] = TI_1
+TI_3['TI'] = TI_2
+
+HI02_1 = list()
+HI02_2 = list()
+for i in range(0, len(HI02)):
+    HI02_1.append(HI02[i]['measDate'])
+    HI02_2.append(HI02[i]['HI02'])
+
+HI02_3 = dict()
+HI02_3['measDate'] = HI02_1
+HI02_3['HI02'] = HI02_2
+
+CI_1 = list()
+CI_2 = list()
+for i in range(0, len(CI)):
+    CI_1.append(CI[i]['measDate'])
+    CI_2.append(CI[i]['CI'])
+
+CI_3 = dict()
+CI_3['measDate'] = CI_1
+CI_3['CI'] = CI_2
+
+df_TI = pd.DataFrame(TI_3, index = TI_3['measDate'])
+df_HI02 = pd.DataFrame(HI02_3, index = HI02_3['measDate'])
+df_CI = pd.DataFrame(CI_3, index = CI_3['measDate'])
+
+df_M = pd.merge(df_TI, df_HI02, how = 'outer')
+df_M = pd.merge(df_M, df_CI, how = 'outer')
+print(df_M)
 
