307cbf5bff74fbb0bf7a3b336b54f673628f071b
JaeHoon Shin <shinj@korea.kr>
2023-04-18 21:46:03 +0900

    Update API_iot_USEM.py

--- pest/API_iot_USEM.py
+++ pest/API_iot_USEM.py
@@ -21,48 +21,52 @@ api_url_r = "http://iot.rda.go.kr/api"
 # api key : read access
 api_key_r = "API_KEY"
 
-# data logging date and time
+#################################################
 
-dt_now = dt.datetime.now()
-dt_end = dt_now
-dt_start = dt_now - dt.timedelta(hours = DELTA)
-dt_now = dt_start
-
-for i in range(0, DELTA):
-    date = dt.datetime.strftime(dt_now, "%Y%m%d")
-    time = dt.datetime.strftime(dt_now, "%H")
-    #print(i, date, time)
-    dt_now = dt_now + dt.timedelta(hours = 1)
-    api_uri = f"{api_url_r}/{api_key_r}/{date}/{time}"
+def get_data(dt_now, DELTA):
+    dt_run = dt_now - dt.timedelta(hours=DELTA)
+    for i in range(0, DELTA):
+        date = dt.datetime.strftime(dt_run, "%Y%m%d")
+        time = dt.datetime.strftime(dt_run, "%H")
+        #print(i, date, time)
+        dt_run = dt_run + dt.timedelta(hours = 1)
+        api_uri = f"{api_url_r}/{api_key_r}/{date}/{time}"
 
 # request url
-    response = requests.get(api_uri, verify=False)
+        response = requests.get(api_uri, verify=False)
 
 # response body
-    json_obj = json.loads(response.text)
+        json_obj = json.loads(response.text)
 
-    custom_dt[i] = json_obj['datas'][0]['custom_dt']
-    datetime[i] = json_obj['datas'][0]['datetime']
-    humidity[i] = json_obj['datas'][0]['humidity']
-    temperature[i] = json_obj['datas'][0]['temperature']
-    leafwet[i] = json_obj['datas'][0]['leafwet']
-    co2[i] = json_obj['datas'][0]['cotwo']
-    soiltemp[i] = json_obj['datas'][0]['gtemperature']
-    quantum[i] = json_obj['datas'][0]['quantum']
+        custom_dt[i] = json_obj['datas'][0]['custom_dt']
+        datetime[i] = json_obj['datas'][0]['datetime']
+        humidity[i] = json_obj['datas'][0]['humidity']
+        temperature[i] = json_obj['datas'][0]['temperature']
+        leafwet[i] = json_obj['datas'][0]['leafwet']
+        co2[i] = json_obj['datas'][0]['cotwo']
+        soiltemp[i] = json_obj['datas'][0]['gtemperature']
+        quantum[i] = json_obj['datas'][0]['quantum']
 
+    usem = dict()
+    usem['datetime'] = datetime
+    usem['temperature'] = temperature
+    usem['humidity'] = humidity
+    usem['leafwet'] = leafwet
+    usem['soiltemp'] = soiltemp
+    usem['co2'] = co2
+    usem['quantum'] = quantum
+    print(temperature)
 
-usem = dict()
-usem['datetime'] = datetime
-usem['temperature'] = temperature
-usem['humidity'] = humidity
-usem['leafwet'] = leafwet
-usem['soiltemp'] = soiltemp
-usem['co2'] = co2
-usem['quantum'] = quantum
+    return(usem)
+
+########################################################
+
+dt_now = dt.datetime.now()
+usem = get_data(dt_now, DELTA)
 
 df_usem = pd.DataFrame(usem)
 #print(df_usem.sort_values("datetime", ascending = True))
-#print(df_usem)
+print(df_usem)
 
 with open('usem.txt', 'w') as file_data:
    file_data.write(str(df_usem.sort_values("datetime", ascending = True)))
