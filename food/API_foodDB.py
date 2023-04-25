79fda8e0c7acab848b787224afa1b1956a357fbe
JaeHoon Shin <shinj@korea.kr>
2023-04-25 23:29:51 +0900

    update

--- food/API_foodDB.py
+++ food/API_foodDB.py
@@ -14,18 +14,19 @@ import requests
 import requests
 
 import json
+import xmltodict
 
 # server URL
 api_url = "https://apis.data.go.kr/1390802/AgriFood/NationStdFood/V3"
 
 # read api key
-api_key = "API KEY"
+api_key = "api key"
 
 api_uri = f"{api_url}/getKoreanFoodNationStdIdntList"
 
 paramDict = {
     'serviceKey': api_key,
-    'food_Code': 'A001001A010a'
+    'food_Code': 'I0170010009a'
 }
 
 print(api_uri)
@@ -35,4 +36,26 @@ headers = {'Accept': 'application/json; charset=utf-8', 'Content-Type': 'applica
 response = requests.get(api_uri, headers=headers, params=paramDict)
 
 # response body
-print(response.text)
+#print(response.text)
+
+dict_obj = xmltodict.parse(response.text)
+json_obj = json.dumps(dict_obj)
+print(json_obj)
+
+# response body
+if dict_obj['response']['header']['result_Code'] == "200":
+    print("###########################")
+    print("food_Code: " + dict_obj['response']['body']['items']['item']['food_Code'])
+    print("food_Grupp: " + dict_obj['response']['body']['items']['item']['food_Grupp'])
+    print("food_Nm: " + dict_obj['response']['body']['items']['item']['food_Nm'])
+    print("food_Eng_Nm: " + dict_obj['response']['body']['items']['item']['food_Eng_Nm'])
+    print("origin_Nm: " + dict_obj['response']['body']['items']['item']['origin_Nm'])
+    print("examin_Year: " + dict_obj['response']['body']['items']['item']['examin_Year'])
+    print("###########################")
+
+    for i in range(0, len(dict_obj['response']['body']['items']['item']['irdnt'])) :
+        print()
+        print("************************ irdnt_Se_Nm: " + dict_obj['response']['body']['items']['item']['irdnt'][i]['irdnt_Se_Nm'])
+        for j in range(0,len(dict_obj['response']['body']['items']['item']['irdnt'][i]['irdnttcket'])):
+            print(dict_obj['response']['body']['items']['item']['irdnt'][i]['irdnttcket'][j]['irdnt_Nm'] + ": " +\
+                  dict_obj['response']['body']['items']['item']['irdnt'][i]['irdnttcket'][j]['cont_Info'])
--- food/API_fooddiet.py
+++ food/API_fooddiet.py
@@ -14,12 +14,13 @@ import requests
 import requests
 
 import json
+import xmltodict
 
 # server URL
 api_url = "http://apis.data.go.kr/1390802/AgriFood/FdFoodCkryImage"
 
 # read api key
-api_key = "API KEY"
+api_key = "api key"
 
 api_uri = f"{api_url}/getKoreanFoodFdFoodCkryImageList"
 
@@ -30,8 +31,8 @@ paramDict = {
     'service_Type': 'xml',
     'Page_No': 1,
     'Page_Size': 1,
-    'food_Name': '김치',
-    'ckry_Name': '조리'
+    'food_Name': '무',
+    'ckry_Name': '국'
 }
 
 print(api_uri)
@@ -41,4 +42,34 @@ headers = {'Accept': 'application/json; charset=utf-8', 'Content-Type': 'applica
 response = requests.get(api_uri, headers=headers, params=paramDict)
 
 # response body
-print(response.text)
+#print(response.text)
+
+dict_obj = xmltodict.parse(response.text)
+json_obj = json.dumps(dict_obj)
+print(json_obj)
+
+# response body
+if dict_obj['response']['header']['result_Code'] == "200":
+    print("\nfd_Code: " + dict_obj['response']['body']['items']['item']['fd_Code'])
+    print("upper_Fd_Grupp_Nm: " + dict_obj['response']['body']['items']['item']['upper_Fd_Grupp_Nm'])
+    print("fd_Nm: " + dict_obj['response']['body']['items']['item']['fd_Nm'])
+    print("fd_Wgh: " + dict_obj['response']['body']['items']['item']['fd_Wgh'])
+    print("ckry_Nm: " + dict_obj['response']['body']['items']['item']['ckry_Nm'])
+    print("ckry_Sumry_Info: " + dict_obj['response']['body']['items']['item']['ckry_Sumry_Info'])
+    print("=========================\n")
+    print("재료수:" + dict_obj['response']['body']['items']['item']['food_Cnt'])
+    print("=========================\n")
+
+    for i in range(0, int(dict_obj['response']['body']['items']['item']['food_Cnt'])):
+        print("\n")
+        print("food_Code: " + dict_obj['response']['body']['items']['item']['food_List']['food'][i]['food_Code'])
+        print("food_Nm: " + dict_obj['response']['body']['items']['item']['food_List']['food'][i]['food_Nm'])
+        print("food_Eng_Nm: " + dict_obj['response']['body']['items']['item']['food_List']['food'][i]['food_Eng_Nm'])
+        print("nation_Std_Food_Grupp_Code_Nm: " + dict_obj['response']['body']['items']['item']['food_List']['food'][i]['nation_Std_Food_Grupp_Code_Nm'])
+        print("origin_Code_Nm: " + dict_obj['response']['body']['items']['item']['food_List']['food'][i]['origin_Code_Nm'])
+        print("food_Wgh: " + dict_obj['response']['body']['items']['item']['food_List']['food'][i]['food_Wgh'])
+        print("allrgy_Info: " + dict_obj['response']['body']['items']['item']['food_List']['food'][i]['allrgy_Info'])
+        print("onslf_Std_Food_Grupp_Nm: " + dict_obj['response']['body']['items']['item']['food_List']['food'][i]['onslf_Std_Food_Grupp_Nm'])
+        print("amplt_Cl_Nm: " + dict_obj['response']['body']['items']['item']['food_List']['food'][i]['amplt_Cl_Nm'])
+        print("food_Image_Address: " + str(dict_obj['response']['body']['items']['item']['food_List']['food'][i]['food_Image_Address']))
+
