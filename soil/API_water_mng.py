22394b8a01c71a03050217c6dbb7c1800259c525
JaeHoon Shin <shinj@korea.kr>
2023-04-29 21:12:32 +0900

    update

--- soil/API_water_mng.py
+++ soil/API_water_mng.py
@@ -9,6 +9,7 @@ import json
 import math
 import urllib.request
 import requests
+import xmltodict
 
 # server URL
 api_url = "http://www.naas.go.kr/unityapi/service/byPass/SoilEnviron/waterPrscrptn"
@@ -30,9 +31,33 @@ print(api_uri)
 
 # request url
 headers = {'Accept': 'application/json; charset=utf-8', 'Content-Type': 'application/json; charset=utf-8'}
-response = requests.get(api_uri, headers=headers, params=paramDict)
+response = requests.get(api_uri, headers=headers, params=paramDict, verify=False)
 
 # response body
-print(response.text)
+#print(response.text)
 
+dict_obj = xmltodict.parse(response.text)
+json_obj = json.dumps(dict_obj)
+#print(json_obj)
+
+if dict_obj['response']['header']['result_Code'] == "200":
+    print(dict_obj['response']['body']['items']['item']['view_Cd'])
+    print(dict_obj['response']['body']['items']['item']['sgg_Nm'])
+    print(dict_obj['response']['body']['items']['item']['crop_Nm'])
+    print(dict_obj['response']['body']['items']['item']['planting_Nm'])
+    print(dict_obj['response']['body']['items']['item']['vapour_Stad_Nm'])
+    print(dict_obj['response']['body']['items']['item']['mth_Nm'])
+    print(dict_obj['response']['body']['items']['item']['in_House'])
+    print(dict_obj['response']['body']['items']['item']['growth_Step_Water_Total'])
+    print(dict_obj['response']['body']['items']['item']['step_Day_Total'])
+    print("======================================")
+    for i in range(0, len(dict_obj['response']['body']['items']['item']['growth_List']['growth_Item'])):
+        print(dict_obj['response']['body']['items']['item']['growth_List']['growth_Item'][i]['growth_Step_Cnt'])
+        print("생육단계:", dict_obj['response']['body']['items']['item']['growth_List']['growth_Item'][i]['step_Nm'])
+        print("생육기간:", dict_obj['response']['body']['items']['item']['growth_List']['growth_Item'][i]['step_Day'])
+        print("생육기 총관개량:", dict_obj['response']['body']['items']['item']['growth_List']['growth_Item'][i]['growth_Step_Water'])
+        print("1일 관개량:", dict_obj['response']['body']['items']['item']['growth_List']['growth_Item'][i]['day_Step_Water'])
+        print("------------------------------------")
+
+#################################################
 
