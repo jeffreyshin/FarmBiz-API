8289f95bc5064418cb57e4f6e959e2e2e42e2b0b
JaeHoon Shin <shinj@korea.kr>
2024-03-04 12:07:53 +0900

    update

--- pest/API_iot_USEM.py
+++ pest/API_iot_USEM.py
@@ -7,8 +7,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import urllib3
 
+import base64
+import time
+
+import zipfile
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def fileToBase64(filepath):
+    fp = open(filepath, "rb")
+    data = fp.read()
+    fp.close()
+    return base64.b64encode(data).decode("utf-8")
 
 #################################################
 
@@ -25,10 +35,10 @@ def get_data(dt_now, DELTA):
     dt_run = dt_now - dt.timedelta(hours=DELTA)
     for i in range(0, DELTA):
         date = dt.datetime.strftime(dt_run, "%Y%m%d")
-        time = dt.datetime.strftime(dt_run, "%H")
-        # print(i, date, time)
+        time0 = dt.datetime.strftime(dt_run, "%H")
+        # print(i, date, time0)
         dt_run = dt_run + dt.timedelta(hours=1)
-        api_uri = f"{api_url_r}/{api_key_r}/{date}/{time}"
+        api_uri = f"{api_url_r}/{api_key_r}/{date}/{time0}"
         print(api_uri)
 
         # request url
@@ -39,7 +49,7 @@ def get_data(dt_now, DELTA):
         json_obj = json.loads(response.text)
 
         custom_dt[i] = json_obj['datas'][0]['custom_dt']
-        datetime[i] = f"{date} {time}00"
+        datetime[i] = f"{date} {time0}00"
         humidity[i] = json_obj['datas'][0]['humidity']
         temperature[i] = json_obj['datas'][0]['temperature']
         leafwet[i] = json_obj['datas'][0]['leafwet']
@@ -70,9 +80,9 @@ api_key_r = input("IOT portal 인증키를 입력하세요: ")
 
 dt_now = dt.datetime.now()
 date = dt.datetime.strftime(dt_now, "%Y%m%d")
-time = dt.datetime.strftime(dt_now, "%H")
-DELTA2 = int(time) % 12
-if int(time) < 12:
+time0 = dt.datetime.strftime(dt_now, "%H")
+DELTA2 = int(time0) % 12
+if int(time0) < 12:
     DELTA2 = DELTA2 + 12
 
 print(DELTA2)
@@ -87,12 +97,70 @@ print(df_usem)
 df_usem.to_csv("./usem.csv", index=False)
 
 # server URL
-api_url_anthracnose = 'http://147.46.206.95:7897/Anthracnose'
-api_url_botrytis = 'http://147.46.206.95:7898/Botrytis'
+api_url_anthracnose = 'https://anthracnose-api.camp.re.kr/Anthracnose'
+api_url_botrytis = 'https://botrytis-api.camp.re.kr/Botrytis'
 
 headers = {'Accept': 'application/json; charset=utf-8', 'Content-Type': 'multipart/form-data; charset=utf-8'}
 file = {'file': open('usem.csv', 'rb')}
 
+# 파일 합치기
+with zipfile.ZipFile('sample.zip', 'w') as myzip:
+    myzip.write('usem.csv')
+
+urlm = api_url_anthracnose
+
+# create session
+url = f"{urlm}/connect"
+apikey = "61cdc660a46f4fcc93004de201c58dff"
+param = {"apiKey": apikey}
+res = requests.post(url=url, json=param)
+jobid = res.content.decode('utf-8')
+print(jobid)
+
+# launch model by session key
+inputfile = fileToBase64("./Sample.zip")
+params = {"apiKey": apikey, "jobid": jobid, "file": inputfile}
+url = 'https://anthracnose-api.camp.re.kr/Anthracnose/launch'
+res = requests.post(url=url, json=params)
+r = res.content.decode('utf-8')
+print(r)
+
+
+# get Status model
+url = 'https://anthracnose-api.camp.re.kr/Anthracnose/getStatus'
+params = { "apiKey" : apikey, "jobid": jobid}
+res = requests.post(url=url, json=params)
+if res.status_code == 200:
+    while True:
+        res = requests.post(str(url), json=params)
+        status = res.content.decode('utf-8')
+        if status == "completed":
+            print("completed")
+            break
+        else:
+            print("running")
+            time.sleep(3)
+
+# get output
+url = 'https://anthracnose-api.camp.re.kr/Anthracnose/getOutput'
+params = { "apiKey" : apikey, "jobid": jobid, "variable" : "all"}
+res = requests.post(url=url, json=params)
+if res.status_code == 200:
+    file_path = 'output.zip'
+    with open(file_path, 'wb') as file:
+        file.write(res.content)
+
+# remove session
+url = 'https://anthracnose-api.camp.re.kr/Anthracnose/disconnect'
+params = { "apiKey" : apikey, "jobid": jobid }
+res = requests.post(str(url), json=params)
+r = res.content.decode('utf-8')
+print(r)
+
+
+
+
+'''
 response = requests.post(api_url_anthracnose, files=file)
 
 r = response.json()
@@ -120,3 +188,4 @@ plt.title("Scatter Plot of the data")
 plt.xlabel("X")
 plt.ylabel("PINF")
 plt.show()
+'''
