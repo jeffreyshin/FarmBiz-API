4070b2142d4fb23a1e155e1cb9527b499dc43979
JaeHoon Shin <shinj@korea.kr>
2024-03-24 13:51:38 +0900

    update

--- weather/API_rda_30mfcst.py
+++ weather/API_rda_30mfcst.py
@@ -10,21 +10,31 @@ import math
 import urllib.request
 import requests
 import xmltodict
+import datetime as dt
 
 # pip install requests
 
 # server URL
 region = 'hadong'
-itemsList = ['tmax', 'tmin', 'rain']
-daysList = ['20230716', '20230717']
-items =','.join(itemsList)
-days = ','.join(daysList)
+daysList = list()
 geocode ='127.72743,35.09714'
-api_url = f"https://hadong.agmet.kr/farm/pickvalue/{region}/{items}/{days}/{geocode}/json"
 
-print(api_url)
+itemsList = ['tmax', 'tmin', 'rain']
+items =','.join(itemsList)
+
+dt_today = dt.datetime.now()
+dt_i = dt_today - dt.timedelta(days=3)
+
+for i in range(0, 7):
+    date = dt.datetime.strftime(dt_i, "%Y%m%d")
+    dt_i = dt_i + dt.timedelta(days=1)
+    daysList.append(date)
+
+days = ','.join(daysList)
 
 # request url
+api_url = f"https://hadong.agmet.kr/farm/pickvalue/{region}/{items}/{days}/{geocode}/json"
+print(api_url)
 headers = {'Accept': 'application/json; charset=utf-8', 'Content-Type': 'application/json; charset=utf-8'}
 response = requests.get(api_url, headers=headers)
 
@@ -33,6 +43,8 @@ dict_obj = response.json()
 
 if response.status_code == 200:
     for j in daysList:
+        print()
         for k in itemsList:
-            print(j, k, dict_obj[k][j][geocode], dict_obj[k][j][geocode], dict_obj[k][j][geocode])
+            if dict_obj[k][j] :
+                print(j, k, dict_obj[k][j][geocode])
 
